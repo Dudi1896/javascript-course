9222cf1f14e9b2b86380aeda2dbca86ed4cb6e47
Dudi1896 <denzelu181@gmail.com>
2023-03-07 01:26:42 -0500

    lab6

---
+++ Lab6/script.js
@@ -0,0 +1,12 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Lab 6
+
+      Project to validate a form used for setting up a new account
+      Author: 
+      Date:   
+
+      
+*/
+
