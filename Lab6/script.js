f0fa7212557baaa565cf782e1032e5194ec75dec
Dudi1896 <denzelu181@gmail.com>
2023-03-24 14:15:15 -0400

    test

--- Lab6/script.js
+++ Lab6/script.js
@@ -4,9 +4,25 @@
       Lab 6
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Denzel Udemba
+      Date:   03/07/23
 
       
 */
 
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function () {
+      if (!pwd.value.match(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)) {
+      alert(`Your password must be at least 8 
+      characters with at least one letter and one number`);
+      }   
+      else if (pwd.value !== pwd2.value) {
+      alert(`Your passwords must match`);
+      }
+      else {
+            alert(``);
+      }
+});
