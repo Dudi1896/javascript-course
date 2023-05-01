93c6e0366f53edcae635d8f45b3dab6f186bbed5
Dudi1896 <denzelu181@gmail.com>
2023-05-01 14:50:35 -0400

    lab12 complete

--- Lab12/script.js
+++ Lab12/script.js
@@ -4,9 +4,24 @@
       
 
       Project to convert between celsius and fahrenheit
-      Author: 
-      Date:   
+      Author: Denzel Udemba   
+      Date:   04/30/23
 
      
-*/
 
+*/
+// Celsius to Fahrenheit
+$(document).ready(function () {
+  $("#cvalue").change(function () {
+    let celsius = $(this).val();
+    let fahrenheit = 1.8 * celsius + 32;
+    $("#fValue").val(fahrenheit.toFixed(0));
+  });
+
+  // Fahrenheit to Celsius
+  $("#fValue").change(function () {
+    let fahrenheit = $(this).val();
+    let celsius = (fahrenheit - 32) / 1.8;
+    $("#cValue").val(celsius.toFixed(0));
+  });
+});
