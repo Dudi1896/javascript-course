0f697938fc8e15910c5405b0ceb9aaba38c29461
Dudi1896 <denzelu181@gmail.com>
2023-05-01 14:19:33 -0400

    Lab11 complete

--- Lab11/script.js
+++ Lab11/script.js
@@ -4,8 +4,8 @@
       
 
       Project to city and state information from a provided postal code
-      Author: 
-      Date:   
+      Author: Denzel Udemba   
+      Date:   04/30/2023
 
       
 */
@@ -14,10 +14,26 @@ let postalCode = document.getElementById("postalCode");
 let place = document.getElementById("place");
 let region = document.getElementById("region");
 let country = document.getElementById("country");
+let longitude = document.getElementById("longitude");
+let latitude = document.getElementById("latitude");
 
-postalCode.onblur = function() {
- 
-}
+postalCode.onblur = function () {
+  let codeValue = postalCode.value;
+  let countryValue = country.value;
+
+  place.value = "";
+  region.value = "";  
+  longitude.value = "";
+  latitude.value = "";
 
 
-
+  fetch(`http://api.zippopotam.us/${countryValue}/${codeValue}`)
+    .then((response) => response.json())
+    .then((json) => {
+      place.value = json.places[0]["place name"];
+      region.value = json.places[0]["state abbreviation"];
+      longitude.value = json.places[0]["longitude"];
+      latitude.value = json.places[0]["latitude"];
+    })
+    .catch((error) => console.log(error));
+};
